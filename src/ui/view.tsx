229d44d4714fce4b48e85d8712b5306ada4119c2
Fabian Morón Zirfas <hash@ff6347.email>
2025-09-07 12:38:23 +0200

    feat: React view scaffold

---
+++ src/ui/view.tsx
@@ -0,0 +1,53 @@
+import { ItemView, WorkspaceLeaf } from "obsidian";
+import { StrictMode } from "react";
+import { type Root, createRoot } from "react-dom/client";
+import { ReactView } from "./components/react-view.js";
+
+export const VIEW_TYPE_CANVAS_CONTEXT = "canvas-context-view";
+
+export class CanvasContextView extends ItemView {
+	root: Root | null = null;
+
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	getViewType() {
+		return VIEW_TYPE_CANVAS_CONTEXT;
+	}
+
+	getDisplayText() {
+		return "Example view";
+	}
+
+	async onOpen() {
+		this.root = createRoot(this.contentEl);
+		this.root.render(
+			<StrictMode>
+				<ReactView />
+			</StrictMode>,
+		);
+
+		// const container = this.contentEl;
+		// container.empty();
+		// container.createEl("h1", { text: "Canvas Context" });
+		// container.createEl("p", { text: "This is an example view." });
+		// I want to have here some options and infos.
+		// basic infos
+		// the current estimated token count
+		// the current provider
+		//  which model is used
+		// we should be able to switch the model and the provider here
+		// Each provider will have some specific view if needed
+		// first will be LM Studio and ollama with a dropdown
+		// then the model selection
+		// we need to be able to refresh the available models
+		// - which options do we surface
+		// - temperature
+		// - max tokens
+	}
+
+	async onClose() {
+		this.root?.unmount();
+	}
+}
