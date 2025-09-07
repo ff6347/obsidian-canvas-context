b5fbba47e8388f2d4e12d691b6ee5b7cabdc25c2
Fabian Morón Zirfas <hash@ff6347.email>
2025-09-07 12:37:54 +0200

    chore: Scaffold settings

---
+++ src/ui/settings.ts
@@ -0,0 +1,30 @@
+import { App, PluginSettingTab, Setting } from "obsidian";
+import type CanvasContextPlugin from "src/main.js";
+
+export class CanvasContextSettingTab extends PluginSettingTab {
+	plugin: CanvasContextPlugin;
+
+	constructor(app: App, plugin: CanvasContextPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.settings)
+					.onChange(async (value) => {
+						this.plugin.settings.settings = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+	}
+}
